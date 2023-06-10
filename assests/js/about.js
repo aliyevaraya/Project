39913f6598b8c4d3381f80b8be452b717df84b3c
Raya Aliyeva <73crd54@code.edu.az>
2023-06-11 03:08:59 +0400

    feat:some changes in happy client  sec

--- assests/js/about.js
+++ assests/js/about.js
@@ -1,13 +1,46 @@
-let valueDisplays = document.querySelectorAll(".quantity");
 
-valueDisplays.forEach((valueDisplay) => {
-  let startValue = 0;
-  let endValue = parseInt(valueDisplay.getAttribute("data-max"));
-  let counter = setInterval(() => {
-    startValue += 1;
-    valueDisplay.innerHTML = startValue;
-    if (startValue == endValue) {
-      clearInterval(counter);
-    }
-  }, 5000/endValue);
+
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
 });
+
+
+// let a = 0;
+// $(window).scroll(function () {
+//   const oTop = $("#statics").offset().top - window.innerHeight;
+//   console.log($("#statics").offset());
+//   if ($(window).scrollTop() > oTop) {
+//     $(".count").each(function () {
+//       const $this = $(this),
+//         countTo = $this.attr("data-count");
+//       $({
+//         countNum: $this.text(),
+//       }).animate(
+//         {
+//           countNum: countTo,
+//         },
+
+//         {
+//           duration: 2500,
+//           easing: "swing",
+//           step: function () {
+//             $this.text(Math.floor(this.countNum));
+//           },
+//           complete: function () {
+//             $this.text(this.countNum);
+//             //alert('finished');
+//           },
+//         }
+//       );
+//     });
+//     // a = 1;
+//   }
+// });
