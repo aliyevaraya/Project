0a75e3797226bb85cac25d3b5cfa588cc3ea8962
Raya Aliyeva <73crd54@code.edu.az>
2023-06-11 15:02:30 +0400

    feat:some changes in about.js

--- assests/js/about.js
+++ assests/js/about.js
@@ -1,1 +1,22 @@
 
+
+let a = 0;
+$(window).scroll(function () {
+  let oTop = $("#statics").offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $(".count").each(function () {
+      let $this = $(this);
+      jQuery({ Counter: 0 }).animate(
+        { Counter: $this.text() },
+        {
+          duration: 2000,
+          easing: "swing",
+          step: function () {
+            $this.text(Math.ceil(this.Counter));
+          },
+        }
+      );
+    });
+    a = 1;
+  }
+});
