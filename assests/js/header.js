db593dc089da805cb2543726250ba5134881be14
Raya Aliyeva <73crd54@code.edu.az>
2023-06-12 23:42:11 +0400

    feat:login signup  added

--- assests/js/header.js
+++ assests/js/header.js
@@ -10,3 +10,23 @@ function scrollFunc() {
 }
 
 window.onscroll = function () { scrollFunc() }
+
+const popUp = document.querySelector('.popup')
+const logIn = document.querySelector('.login-link')
+const register = document.querySelector('.register-link')
+const closePopUp = document.querySelector('.icon-close')
+const openPopUp = document.querySelector('.btnLogin-popup')
+
+register.addEventListener("click",()=>{
+    console.log('hi');
+    popUp.classList.add("active")
+})
+logIn.addEventListener("click",()=>{
+    popUp.classList.remove("active")
+})
+closePopUp.addEventListener("click",()=>{
+    popUp.style.opacity=0
+})
+openPopUp.addEventListener("click",()=>{
+    popUp.style.opacity=1
+})
