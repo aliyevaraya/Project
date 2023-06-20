f6af23b7cbc300fa374e7939549ffd6ba5395b47
Raya Aliyeva <73crd54@code.edu.az>
2023-06-20 23:29:40 +0400

    feat: add cart added to favs

--- assests/js/favorites.js
+++ assests/js/favorites.js
@@ -12,15 +12,71 @@ function drawFav() {
       <h6>${fav.type}</h6>
       <p class="price">$${fav.price}</p>
       <a href="#" onclick=delFav(${fav.id},this) class="btn remove">Remove Fav</a>
+      <a href="#" onclick=addCart(${fav.id},this) class="btn btn-success mt-2">Add Cart</a>
     </div>
   </div>
         `;
   });
 }
-drawFav()
+drawFav();
 function delFav(id, btn) {
   favs = favs.filter((user) => user.id != id);
   localStorage.setItem("Favorites", JSON.stringify(favs));
   btn.closest(".fav").remove();
   drawFav();
+}
+const PRODUCTS_URL = "http://localhost:8080/products";
+let myCart = JSON.parse(localStorage.getItem("My_Cart")) || [];
+let quantity = document.querySelector(".number");
+quantity.innerHTML = myCart.length;
+
+async function getCard() {
+  try {
+    const res = await axios(PRODUCTS_URL);
+    const data = res.data;
+    copyArr = data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+getCard();
+
+function getMyCart() {
+  let inCart = JSON.parse(localStorage.getItem("My_Cart")) || [];
+  ul.innerHTML = "";
+  if (inCart.length == 0) {
+    ul.innerHTML = "You cart is empty";
+  } else {
+    inCart.slice(0, 3).forEach((prod) => {
+      ul.innerHTML += `
+    <li class="incart">
+              <div class="cart-info">
+                <div class="img">
+                  <img src="${prod.photo}" alt="" />
+                </div>
+                <div class="info">
+                  <p class="prod-name">${prod.name}</p>
+                  <p class="prod-type">${prod.type}</p>
+                  <span class="prod-price">$${prod.price}.00</span>
+                </div>
+              </div>
+              <button onclick=removerProd(${prod.id})><i class="fa-solid fa-xmark remove-prod"></i></button>
+            </li>
+    `;
+    });
+  }
+}
+
+async function addCart(id) {
+  let myCart = JSON.parse(localStorage.getItem("My_Cart")) || [];
+  prod = copyArr.find((prod) => prod.id == id);
+  let check = myCart.find((item) => item.id == prod.id);
+  if (!check) {
+    myCart.push(prod);
+    localStorage.setItem("My_Cart", JSON.stringify(myCart));
+    quantity.innerHTML = Number(quantity.innerHTML) + 1;
+    getMyCart();
+  } else {
+    prod.quantity += 1;
+  }
 }
